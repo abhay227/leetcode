7eec8e1134f2d331bc1e490d3f9512b4eb350a97
abhay227 <abhaypandey264209@gmail.com>
2024-08-08 18:48:12 +0530

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+    <>
+				<title>LeetClone</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.png' />
+				<meta
+					name='description'
+					content='Web application that contains leetcode problems and video solutions'
+				/>
+		</>
+
+  <Component {...pageProps} />;
+
+  </>
+  )
 }
